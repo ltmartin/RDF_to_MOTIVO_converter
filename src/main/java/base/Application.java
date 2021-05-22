772f413fe4ca63cd2872c7aaaa46c344e31d5a5e
Leandro Tabares Martín <ltmartin@uci.cu>
2021-05-22 09:56:16 +0200

    Dockerfile completed.

--- src/main/java/base/Application.java
+++ src/main/java/base/Application.java
@@ -1,15 +1,27 @@
 package base;
 
+import base.converters.RdfToMotivoConverter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
 
 /**
  * @author Leandro Tabares Martín
  *
  **/
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+
+    @Resource
+    private RdfToMotivoConverter converter;
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    public void run(String... args) {
+        converter.convert();
     }
 }
